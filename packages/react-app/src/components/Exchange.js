654461632163657b3f24a9f2351eb8239b538546
Volodymyr <volodyaruzhak@gmail.com>
2023-02-15 18:25:27 -0500

    create 3 new components; working on exchange as well

--- packages/react-app/src/components/Exchange.js
+++ packages/react-app/src/components/Exchange.js
@@ -1,4 +1,4 @@
-import React from 'react';
+// import React from 'react';
 import React, { useEffect, useState } from 'react';
 import { Contract } from '@ethersproject/contracts';
 import { abis } from '@my-app/contracts';
@@ -15,8 +15,69 @@ import { ROUTER_ADDRESS } from '../config';
 
 const Exchange = () => {
     return (
-        <div>Exchange</div>
-    )
+        <div className='flex flex-col w-full items-center'>
+            <div className='mb-8'>
+                <AmountIn
+                    value={fromValue}
+                    onChange={onFromValueChange}
+                    currencyValue={fromToken}
+                    onSelect={onFromTokenChange}
+                    currencies={availableTokens}
+                    isSwapping={isSwapping && hasEnoughBalance}
+                />
+                <Balance tokenBalance={fromTokenBalance} />
+            </div>
+
+            <div className='mb-8 w-[100%]'>
+                <AmountOut
+                    fromToken={fromToken}
+                    toToken={toToken}
+                    amountIn={fromValueBigNumber}
+                    pairContract={pairAddress}
+                    currencyValue={toToken}
+                    onSelect={onToTokenChange}
+                    currencies={counterpartTokens}
+                />
+                <Balance tokenBalance={toTokenBalance} />
+            </div>
+
+            {approvedNeeded && !isSwapping ? (
+                <button
+                    disabled={!canApprove}
+                    onClick={onApproveRequested}
+                    className={`${
+                        canApprove
+                        ? 'bg-site-pink text-white'
+                        : 'bg-site-dim2 text-site-dim2'
+                    } ${styles.actionButton}`}
+                    >
+                    {isApproving ? 'Approving...' : 'Approve'}
+                </button>
+            ) : (
+                <button
+                    disabled={!canSwap}
+                    onClick={onSwapRequested}
+                    className={`${
+                        canSwap ? 'bg-site-pink text-white' : 'bg-site-dim2 text-site-dim2'
+                    } ${styles.actionButton}`}
+                    >
+                    {isSwapping
+                        ? 'Swapping...'
+                        : hasEnoughBalance
+                        ? 'Swap'
+                        : 'Insufficient balance'}
+                </button>
+            )}
+
+            {failureMessage && !resetState ? (
+                <p className={styles.message}>{failureMessage}</p>
+            ) : successMessage ? (
+                <p className={styles.message}>{successMessage}</p>
+            ) : (
+                ""
+            )}
+        </div>
+    );
 }
 
 export default Exchange
