20deb7a46fc34b82ab7e5e6bb4bc247748ae9107
Volodymyr <volodyaruzhak@gmail.com>
2023-02-15 13:43:55 -0500

    simple loader completed

--- packages/react-app/src/components/Loader.js
+++ packages/react-app/src/components/Loader.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from 'react';
+import styles from '../styles';
+import { ethereumLogo } from '../assets';
 
-const Loader = () => {
+const Loader = ({ title }) => {
     return (
-        <div>Loader</div>
-    )
-}
+        <div className={styles.loader}>
+            <img src={ethereumLogo} alt='ethereum logo' className={styles.loaderImg} />
+            <p className={styles.loaderText}>{title}</p>
+        </div>
+    );
+};
 
-export default Loader
+export default Loader;
